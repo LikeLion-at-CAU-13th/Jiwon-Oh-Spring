54fe45d41cf6328babb5a2990b8d7992ad4b9305
ziwonii <nayanako03@gmail.com>
2025-07-13 02:02:50 +0900

    feat: health check API

---
+++ src/main/java/com/example/likelion13th_spring/Controller/HealthCheckController.java
@@ -0,0 +1,13 @@
+package com.example.likelion13th_spring.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/health")
+    public String healthCheck() {
+        return "OK";
+    }
+}
